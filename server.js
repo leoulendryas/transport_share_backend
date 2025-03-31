f7d0b88b1957ac1004d067c887e7399fcae36494
leoulendryas <leoulendryas@gmail.com>
2025-03-31 17:46:21 +0300

    Added server.js

---
+++ server.js
@@ -0,0 +1,5 @@
+require('dotenv').config();
+const app = require('./app');
+
+// The app.js already handles the server creation and listening
+// This file is just for clarity and potential future extensions
